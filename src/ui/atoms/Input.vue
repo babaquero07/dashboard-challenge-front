794c34e96ae64172574c4e091ad11f36fdbbaade
Alexander Baquero <brayanalexanderb@gmail.com>
2025-06-21 16:56:00 -0500

    style: update Input component styling and modify global styles for improved light mode support

--- src/ui/atoms/Input.vue
+++ src/ui/atoms/Input.vue
@@ -27,7 +27,7 @@ const handleInput = (event: Event) => {
       :autocomplete="name"
       @input="handleInput"
       required
-      class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+      class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
       :placeholder="placeholder"
     />
   </div>

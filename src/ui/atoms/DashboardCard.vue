78cae66a6284aa68feb3671949c364ea1c852cb3
Alexander Baquero <brayanalexanderb@gmail.com>
2025-06-21 18:23:43 -0500

    feat(ui): add DashboardCard component for displaying individual dashboards with click functionality

---
+++ src/ui/atoms/DashboardCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  dashboardName: string;
+  handleClick: () => void;
+}>();
+</script>
+
+<template>
+  <li
+    class="min-w-[200px] text-center border border-solid border-gray-700 p-4 rounded-md bg-indigo-400 hover:bg-indigo-500 transition-all duration-300 text-white font-semibold cursor-pointer ease-in-out"
+    @click="handleClick"
+  >
+    <h2>{{ dashboardName }}</h2>
+  </li>
+</template>

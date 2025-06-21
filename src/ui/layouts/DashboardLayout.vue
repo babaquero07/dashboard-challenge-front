258218073272591aec7b7c38f7954b6c7535b1be
Alexander Baquero <brayanalexanderb@gmail.com>
2025-06-21 16:56:11 -0500

    feat(ui): add DashboardLayout and Navbar components for improved user navigation and layout structure

---
+++ src/ui/layouts/DashboardLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Navbar from "../molecules/Navbar.vue";
+</script>
+
+<template>
+  <header class="w-full bg-white shadow-md">
+    <Navbar />
+  </header>
+
+  <main class="w-full p-4">
+    <slot name="content" />
+  </main>
+</template>
---
+++ src/ui/molecules/Navbar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { useAuthStore } from "../../stores/auth";
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+const handleLogout = () => {
+  authStore.logout();
+  router.push("/login");
+};
+</script>
+
+<template>
+  <nav class="w-full bg-white px-8 py-4 border-b border-gray-200">
+    <ul class="flex items-center justify-between">
+      <li>
+        <RouterLink to="/dashboard">
+          <span
+            class="text-[20px] font-bold text-gray-800 hover:text-indigo-700"
+            >Dashboard</span
+          >
+        </RouterLink>
+      </li>
+      <li @click="handleLogout">
+        <span
+          class="text-gray-900 hover:text-indigo-700 cursor-pointer font-medium"
+          >Cerrar sesión</span
+        >
+      </li>
+    </ul>
+  </nav>
+</template>

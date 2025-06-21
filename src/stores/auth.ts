c3cc641369b91b5ec81c296aecd9811ee88595a8
Alexander Baquero <brayanalexanderb@gmail.com>
2025-06-21 14:15:39 -0500

    feat(store): integrate Pinia for state management and add authentication interfaces

---
+++ src/stores/auth.ts
@@ -0,0 +1,35 @@
+import { defineStore } from "pinia";
+import type { User } from "../interfaces/general";
+import type { LoginData } from "../interfaces/auth";
+import { AuthServices } from "../api/services/Auth/AuthServices";
+
+const authService = new AuthServices();
+
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    user: null as User | null,
+    token: localStorage.getItem("token") || null,
+  }),
+  getters: {
+    isAuthenticated: (state) => !!state.token,
+  },
+  actions: {
+    async login(loginData: LoginData) {
+      try {
+        const { user, token } = await authService.login(loginData);
+        this.user = user;
+        this.token = token;
+
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+      } catch (error) {
+        throw error;
+      }
+    },
+    logout() {
+      this.user = null;
+      this.token = null;
+      localStorage.removeItem("token");
+    },
+  },
+});

13f2b5d4b536acade6a0c2692d2d109cacb3c053
Alexander Baquero <brayanalexanderb@gmail.com>
2025-06-21 16:30:59 -0500

    feat(router): add registration route and integrate RegisterView component for user registration

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,8 @@ import {
   type RouteRecordRaw,
 } from "vue-router";
 import LoginView from "../ui/views/LoginView.vue";
+import { useAuthStore } from "../stores/auth";
+import RegisterView from "../ui/views/RegisterView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,11 +20,31 @@ const routes: Array<RouteRecordRaw> = [
       requiresAuth: false, // Public route
     },
   },
+  {
+    path: "/register",
+    name: "Register",
+    component: RegisterView,
+    meta: {
+      requiresAuth: false, // Public route
+    },
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+// router.beforeEach((to, from, next) => {
+//   const authStore = useAuthStore();
+
+//   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+//     next({ name: "Login" });
+//   } else if (to.name === 'login' && authStore.isAuthenticated) {
+//     next('/dashboard')
+//   } else {
+//     next();
+//   }
+// });
 
 export default router;

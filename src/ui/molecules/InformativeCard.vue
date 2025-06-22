0b386ae023458321acb63a0b51b8158a7edc3555
Alexander Baquero <brayanalexanderb@gmail.com>
2025-06-21 20:51:35 -0500

    feat(ui): add InformativeCard component for displaying key metrics with title, value, and percentage change

---
+++ src/ui/molecules/InformativeCard.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  value: number;
+  percentage: number;
+}>();
+</script>
+
+<template>
+  <div
+    class="flex flex-col gap-2 p-4 bg-white rounded-md shadow-md w-full h-full"
+  >
+    <h3 class="text-sm font-medium text-gray-500">{{ title }}</h3>
+    <p class="text-2xl font-bold text-gray-900">
+      ${{ value.toLocaleString() }}
+    </p>
+    <p class="text-sm text-gray-500">
+      {{ percentage > 0 ? "+" : "" }}{{ percentage }}% respecto al mes anterior
+    </p>
+  </div>
+</template>
